4307d86a37250c362eba41d94daca654286d6f5b
Son,Haegwang <sonhaiguang21@gmail.com>
2024-09-25 09:29:19 +0900

    third

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) {
-      System.out.println("손해광");
-    }
-  
+  public static void main(String[] args) {
+    System.out.println("홍길동");
+    World.run();
   }
-  
+
+}
